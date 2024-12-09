d776af41dd94418c8954cad6b984668a89928b7a
DoloresM <DoloresM@users.noreply.github.com>
2024-12-08 23:58:54 -0500

    debugging needed. home value not found.

--- src/main/java/io/newarkjoggers/runners/run/RunController.java
+++ src/main/java/io/newarkjoggers/runners/run/RunController.java
@@ -1,5 +1,16 @@
 package io.newarkjoggers.runners.run;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//annotation to indicate it's a REST controller - which inherently expects JSON response body
+@RestController
 public class RunController {
+
+    //indicating which type of rest controller and rout(web page) to execute code on
+    @GetMapping("/hello")
+    String home(){
+        return "Hello, Newark Joggers!";
+    };
 
 }
